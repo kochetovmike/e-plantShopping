455ac58334928e78e5a5cbe579bc5464f22571f1
Mikhail Kochetov <mike.kochetov@outlook.com>
2025-01-22 10:57:15 -0500

    first 2 steps (may be incorrect)

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,9 +1,20 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { addItem } from './CartSlice';
+
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const [addedToCart, setAddedToCart] = useState({});
+
+    const handleAddToCart = (product) => {
+        dispatchEvent(addItem(product));
+        setAddedToCart((prevState) => ({
+            ...prevstate,
+            [product.name]: true,
+        }))
+    }
 
     const plantsArray = [
         {
@@ -268,8 +279,23 @@ const handlePlantsClick = (e) => {
         </div>
         {!showCart? (
         <div className="product-grid">
-
-
+            {plantsArray.map((item, index) => (
+            
+            <div key={index}>
+                <h1><div>{category.category}</div></h1>
+                <div className="productList">
+                    {category.plants.map((plant, plantIndex) => (
+                        <div className='product-card' key={plantIndex}>
+                            <img className='product-image' src={plant.image} alt={plant.name} />
+                            <div className='product-title'>{plant.name}</div>
+                            <div className='product-card'>{plant.description}</div>
+                            <div className='product-price'>{plant.cost}</div>
+                            <button className='product-button' onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        ))}
         </div>
  ) :  (
     <CartItem onContinueShopping={handleContinueShopping}/>
